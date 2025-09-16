3e57c77cf6bc747adb77f3ee85438454a0dbf7e8
Chroma <110534939+uziff@users.noreply.github.com>
2025-09-16 09:58:35 +0200

    Add fix

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -444,35 +444,32 @@ class Downloader:
         ):
             logger.error("Song is missing required fields: %s", song.display_name)
             self.errors.append(f"Song is missing required fields: {song.display_name}")
+
             return song, None
 
-        # Reinitialize the song object if it's missing metadata
-        # Or if we are fetching albums
-        if (
-            (song.name is None and song.url)
-            or self.settings["fetch_albums"]
-            or any(
-                x is None
-                for x in [
-                    song.genres,
-                    song.disc_count,
-                    song.tracks_count,
-                    song.track_number,
-                    song.album_id,
-                    song.album_artist,
-                ]
+        reinitialized = False
+        try:
+            # Create the output file path
+            output_file = create_file_name(
+                song=song,
+                template=self.settings["output"],
+                file_extension=self.settings["format"],
+                restrict=self.settings["restrict"],
+                file_name_length=self.settings["max_filename_length"],
             )
-        ):
+
+        except Exception:
             song = reinit_song(song)
 
-        # Create the output file path
-        output_file = create_file_name(
-            song=song,
-            template=self.settings["output"],
-            file_extension=self.settings["format"],
-            restrict=self.settings["restrict"],
-            file_name_length=self.settings["max_filename_length"],
-        )
+            output_file = create_file_name(
+                song=song,
+                template=self.settings["output"],
+                file_extension=self.settings["format"],
+                restrict=self.settings["restrict"],
+                file_name_length=self.settings["max_filename_length"],
+            )
+
+            reinitialized = True
 
         if song.explicit is True and self.settings["skip_explicit"] is True:
             logger.info("Skipping explicit song: %s", song.display_name)
@@ -480,6 +477,8 @@ class Downloader:
 
         # Initialize the progress tracker
         display_progress_tracker = self.progress_handler.get_new_tracker(song)
+
+        audio_downloader = None
 
         try:
             # Create the temp folder path
@@ -498,7 +497,7 @@ class Downloader:
             ]
 
             # Checking if file already exists in all subfolders of output directory
-            file_exists = output_file.exists() or dup_song_paths
+            file_exists = file_exists = output_file.exists() or dup_song_paths
             if not self.settings["scan_for_songs"]:
                 for file_extension in self.scan_formats:
                     ext_path = output_file.with_suffix(f".{file_extension}")
@@ -537,6 +536,28 @@ class Downloader:
 
                 display_progress_tracker.notify_download_skip()
                 return song, output_file
+
+            # Check if we have all the metadata
+            # and that the song object is not a placeholder
+            # If it's None extract the current metadata
+            # And reinitialize the song object
+            # Force song reinitialization if we are fetching albums
+            # they have most metadata but not all
+            if (
+                (song.name is None and song.url)
+                or (self.settings["fetch_albums"] and reinitialized is False)
+                or None
+                in [
+                    song.genres,
+                    song.disc_count,
+                    song.tracks_count,
+                    song.track_number,
+                    song.album_id,
+                    song.album_artist,
+                ]
+            ):
+                song = reinit_song(song)
+                reinitialized = True
 
             # Don't skip if the file exists and overwrite is set to force
             if file_exists and self.settings["overwrite"] == "force":
@@ -718,7 +739,7 @@ class Downloader:
                     bitrate = (
                         f"{int(download_info['abr'])}k"
                         if download_info.get("abr")
-                        else "128k"
+                        else "copy"
                     )
                 elif self.settings["bitrate"] == "disable":
                     bitrate = None
@@ -861,3 +882,6 @@ class Downloader:
                 f"{song.url} - {exception.__class__.__name__}: {exception}"
             )
             return song, None
+        finally:
+            if audio_downloader:
+                audio_downloader.close()
